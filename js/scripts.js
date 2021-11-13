737df65fefb8c1234e1e0660a972a42a41b77e0b
Cassel-Evens <casselke@gmail.com>
2021-11-13 14:35:54 -0800

    add if statements to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,34 @@
 $(document).ready(function() {
   $("form#questions").submit(function(event) {
-    const age = parseInt($("input#number").val());
+    event.preventDefault();
+    const number = parseInt($("input#number").val());
     const name = $("input#name").val();
     const animal = $("select#animals").val();
-    const animal = $("select#Colors").val();
-
+    const colors = $("select#Colors").val();
+    const button = $("#btn").val();
     
+    $('#btn[name=btn')
+    .click(
+      function () {
+        $("#survey").hide();
+      }
+    );
 
-
-      // $("#rate").text(quote);
-      // $("#quote").show();
-
-    event.preventDefault();
+    if (animals === 'Snake' && number > 7 || Color === 'Green') {
+      $("#python").show();
+      $("#language").show();
+    } 
+    if (animals === 'Bunnie' && Color === 'Red') {
+      $("ruby").show();
+      $("#language").show();
+    }
+    if (animals === 'Zebra' && number < 1) {
+      $("#Java").show().
+      $("#language").show();
+    } 
+    if (Color === 'Blue' || number > 67) {
+      $("#Java").show();
+      $("#language").show()
+    }
   });
 });
