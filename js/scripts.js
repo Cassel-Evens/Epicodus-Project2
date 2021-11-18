98a2d88f7180562c50fa63257ff195ac7d95abb2
Cassel-Evens <casselke@gmail.com>
2021-11-17 20:04:50 -0800

    add finished functioning java script

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,63 @@
 $(document).ready(function() {
   $("form#questions").submit(function(event) {
+    console.log('work');
     event.preventDefault();
     const number = parseInt($("input#number").val());
-    const name = $("input#name").val();
-    const animal = $("select#animals").val();
+    const animals = $("select#animals").val();
     const colors = $("select#Colors").val();
-    const button = $("#btn").val();
-    
-    $('#btn[name=btn')
-    .click(
-      function () {
-        $("#survey").hide();
-      }
-    );
 
-    if (animals === 'Snake' && number > 7 || Color === 'Green') {
-      $("#python").show();
+
+    if (animals === 'Snake' && number < 7 && colors === 'Green') {
+        $("#python").show();
+        $("#language").show();
+        $("#surveys").hide();
+        $(".header").hide();
+    } else if (animals === 'Bunnie' && colors === 'Red' && number < 6) {
+      $("#ruby").show();
       $("#language").show();
-    } 
-    if (animals === 'Bunnie' && Color === 'Red') {
-      $("ruby").show();
-      $("#language").show();
-    }
-    if (animals === 'Zebra' && number < 1) {
+      $("#surveys").hide();
+      $(".header").hide();
+    } else if (animals === 'Zebra' && colors === 'Red' && number > 6) {
       $("#Java").show().
       $("#language").show();
-    } 
-    if (Color === 'Blue' || number > 67) {
+      $("#surveys").hide();
+      $(".header").hide();
+    } else if (animals === 'Bunnie' && colors === 'Blue' && number > 45) {
       $("#Java").show();
-      $("#language").show()
+      $("#language").show();
+      $("#surveys").hide();
+      $(".header").hide();
+    } else if (animals === 'Snake' && colors === 'Green' && number > 200) {
+      $("#python").show();
+      $("#language").show();
+      $("#surveys").hide();
+      $(".header").hide();
+    } else if(animals === 'Zebra' && colors === 'Blue' && number > 1) {
+      $("#ruby").show();
+      $("#language").show();
+      $("#surveys").hide();
+      $(".header").hide();
+    } else if (colors === 'Blue') {
+      $("#Java").show();
+      $("#language").show();
+      $("#surveys").hide();
+      $(".header").hide();
+    } else if (colors === 'Green') {
+      $("#python").show();
+      $("#language").show();
+      $("#surveys").hide();
+      $(".header").hide();
+    } else if (colors === 'Red') {
+      $("#ruby").show();
+      $("#language").show();
+      $("#surveys").hide();
+      $(".header").hide();
     }
   });
+// });
+// });
 });
+
+function refresh(){
+  window.location.reload("Done.")
+}
